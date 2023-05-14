7186937cdac0f2a405024b272ca146f2f59488e1
Savon <savonstephens4@gmail.com>
2023-05-14 13:36:58 -0700

    made a mistake and deleted my first draft of the assignment

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// youve declared your variable and given it a name of "name" and a value of "dane"
+// conditional states if name is true to be mary then console log Hi mary
+// else console log how do you do
+//hence the first condition isnt true it goes to the next one. so the console will log "how do you do"
 
 //CODE
 /*
@@ -45,10 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// youve declared two variables. one with a value and one without
+// conditional states if code is true to be 123 then secrets new value will be "super" and codes will be "code *2"
+// hence this is true the first conditional will apply 
+//the next conditonal states if code is greater than 250 secrets value will become "duper" hence this isnt true it wont apply
+//the only thing in the console will be secret since the log is seperate from the code blocks
 
 //CODE
 /*
@@ -70,10 +71,12 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// you have three variables declared all with names and values 
+//conditional states if isStudent is exactly "true" and zip is greater than 80000 console log "youre a student on the west coast"
+// else if isStudent is true to be false or age is less than 30 console log "what are your hobbies" 
+//this will log since age is less than 30
+//else if isStudent is exact to true console log "welcome to prime" this log will overide the other since its next in the code block
+//else if isStudent isnt exact to true console log "how about the weather" this will not log since isStudent is true
 
 //CODE
 /*
@@ -129,11 +132,12 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
+//FIX colorOne and colorTwo's values are mixed and in the wrong places 
 /*
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
-
+//FIX you want to set both values to purple with this conditional but you only have it so that colorOne will change and not the other 
 if (mix === true) {
   colorOne = 'purple';
 }
@@ -143,10 +147,11 @@ if (mix === true) {
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
+//FIX you say we started with two varibales but its only one while the other is a constant
 /*
 let temp = 40;
 const time = 4;
-
+//FIX you want check both of these variabls with the "&&" operator instead. Here you used the or operator which will check one or the other not both
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
@@ -155,11 +160,12 @@ if (temp > 39 || time >= 4) {
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
-
+//FIX these arent two variables as one of them is a constant
 /*
 let age = 21;
 const minAge = 21;
-
+//FIX You said you want to check if age is greater than or equal to minAge, but here youre checking if minAge is less than or equal to age
+// it should look like this if(age >= minAge)
 if(minAge <= age) {
   console.log('no entry');
 } else {
